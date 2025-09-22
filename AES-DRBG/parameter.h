170fe116b316c51d2dda709edcd7fb10107c2c10
SeongJun Choi <sjchoi@ranix.co.kr>
2025-09-22 12:05:13 +0900

    모든 security level에서의 RD_ADD, RD_AND RD_ADD2, RD_AND2 오류 수정 완료

--- AES-DRBG/parameter.h
+++ AES-DRBG/parameter.h
@@ -5,7 +5,7 @@
 #define LOG_LWE_N 8             // log dim
 #define LWE_N (1 << LOG_LWE_N)  // LWE dim and LWR dim
 
-#define SMAUG_MODE 5
+#define SMAUG_MODE 1
 #define RAND 1          // 0 -> window rand, 1 -> 직접 입력 
 #define MUL_MOD 0       // 0 -> Original   , 1 -> NTT
 
@@ -23,6 +23,14 @@
 #define LOG_P2 5                // ciphertext2 modulus
 #define HS 70                  // Hamming weight of coefficient vector s
 
+// 반올림을 위한 값 LOG_P는 상위 LOG_P bit 만큼을 남기겠다는 의미이기 때문에
+// LOG_P = 8 이라는 것은 상위 8bit만 사용할 거라는 말임
+// 여기에서의 반올림의 기준은 버리는 애들중 가장 높은 자리 즉, 2^7임
+// 이걸 일반화 하게 된다면, 
+// 반올림 기준    : 버릴 비트 수 16 - LOG_P
+#define RD_ADD 0x80             // 반올림 기준값  : 2(16 - LOG_P) - 1 = 2^(15 - LOG_P)           
+#define RD_AND 0xff00           // 남는 비트수    : 전체 비트 - 반올림 기준 2^16 - 2^(16 - LOG_P)
+
 #define RD_ADD2 0x0400          // 2^(15 - LOG_P2)
 #define RD_AND2 0xf800          // 2^16 - 2^(16 - LOG_P2)
 
@@ -39,8 +47,11 @@
 #define LOG_P2 4                // ciphertext2 modulus
 #define HS 88                  // Hamming weight of coefficient vector s
 
-#define RD_ADD2 0x0080          // 2^(15 - LOG_P2)
-#define RD_AND2 0xff00          // 2^16 - 2^(16 - LOG_P2)
+#define RD_ADD 0x40			    // 2^(15 - LOG_P)
+#define RD_AND 0xff80           // 2^16 - 2^(16 - LOG_P)
+
+#define RD_ADD2 0x0800          // 2^(15 - LOG_P2)
+#define RD_AND2 0xf000          // 2^16 - 2^(16 - LOG_P2)
 
 #elif SMAUG_MODE == 5
 #define SMAUG_NAMESPACE(s) cryptolab_smaug5_##s
@@ -55,17 +66,13 @@
 #define LOG_P2 7                // ciphertext2 modulus
 #define HS 87                  // Hamming weight of coefficient vector s
 
-#define RD_ADD2 0x0200          // 2^(15 - LOG_P2)
-#define RD_AND2 0xfc00          // 2^16 - 2^(16 - LOG_P2)
-#endif
+#define RD_ADD 0x40			    // 2^(15 - LOG_P)
+#define RD_AND 0xff80           // 2^16 - 2^(16 - LOG_P)
 
-// 반올림을 위한 값 LOG_P는 상위 LOG_P bit 만큼을 남기겠다는 의미이기 때문에
-// LOG_P = 8 이라는 것은 상위 8bit만 사용할 거라는 말임
-// 여기에서의 반올림의 기준은 버리는 애들중 가장 높은 자리 즉, 2^7임
-// 이걸 일반화 하게 된다면, 
-// 반올림 기준    : 버릴 비트 수 16 - LOG_P
-#define RD_ADD 0x80             // 반올림 기준값  : 2(16 - LOG_P) - 1 = 2^(15 - LOG_P)           
-#define RD_AND 0xff00           // 남는 비트수    : 전체 비트 - 반올림 기준 2^16 - 2^(16 - LOG_P)
+#define RD_ADD2 0x0100          // 2^(15 - LOG_P2)
+#define RD_AND2 0xfe00          // 2^16 - 2^(16 - LOG_P2)
+
+#endif
 
 #define LOG_T 1                     // plaintext modulus
 #define T (1 << LOG_T)              // binary
